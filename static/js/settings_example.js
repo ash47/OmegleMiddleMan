3a840a43fb5cb6615199387f6a1ba01c87dcd425
ash47 <ash.fourtyseven@gmail.com>
2014-10-30 20:45:21 +1100

    added info in the setting_example.js

--- static/js/settings_example.js
+++ static/js/settings_example.js
@@ -2,6 +2,7 @@
 omegleSettings = {
     // Default topics
     defaultTopics: [
+        'noMultiRP ',   // This topic will cause the bots to disconnect if they connect to each other
         'rp',
         'roleplay'
     ].join(),
